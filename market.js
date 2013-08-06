6efdeac8a05780907d8668d550a76e5587372dc2
Vitaliy <vit@v1t.su>
2013-08-06 15:25:16 +0400

    0.4.2
    Маркет
    + кнопка выбрать все лоты
    
    страница значков
    + фильтр - показать только с невыпавшими картами
    
    значек/карты
    + у отсутсвующих карт кнопка для поиска карты на маркете
    
    Инвентарь
    * фиксы багов связанные с выставлением на продажу группы предметов
    * кнопка поиска предмета на маркете теперь есть и в чужих инвентаря�
    
    * и другие мелкие исправления

--- market.js
+++ market.js
@@ -26,36 +26,17 @@ function mainPage(tabContentsMyListings){
 	document.body.insertAdjacentHTML("afterBegin", 
 		'<style>.scrollbl_listing{max-height:500px;overflow-y:scroll;} .lfremove{display:inline-block}</style>'
 	);
-	/* for future | DON'T WORK, after removal item have new id
-	document.body.insertAdjacentHTML("afterBegin", 
-		'		<div id="market_sell_dialog" class="market_modal_dialog" style="display: none;"><div class="market_dialog_title"><span id="market_sell_dialog_title">Put an item up for sale</span><span class="market_dialog_cancel"><a id="market_sell_dialog_cancel" href="#" class="market_dialog_title_cancel">Cancel<span class="market_dialog_title_cancel_X">X</span></a></span></div><div class="market_dialog_contents"><div class="market_dialog_content_frame"><div class="market_dialog_content"><div id="market_sell_dialog_item_availability_hint"><div class="market_dialog_topwarning"></div></div><div class="itemHolder" style="float: left; margin: 0 15px 0 0;"><div id="market_sell_dialog_item" class="item"><img id="market_sell_dialog_item_img" src="http://cdn.steamcommunity.com/public/images/trans.gif" /></div></div><div class="market_dialog_iteminfo"><div class="market_dialog_itemname hover_item_name" id="market_sell_dialog_item_name"></div><div class="item_desc_game_info" id="market_sell_dialog_game_info"><div class="item_desc_game_icon"><img id="market_sell_dialog_game_icon" src="http://cdn.steamcommunity.com/public/images/trans.gif" /></div><div id="market_sell_dialog_game_name" class="ellipsis"></div><div id="market_sell_dialog_item_type" class=""></div></div></div><div style="clear: left"></div></div><div class="market_dialog_content_separator"></div><div class="market_dialog_content market_dialog_content_dark"><div id="market_sell_dialog_input_area" class="market_sell_dialog_input_area"><div><span id="market_sell_quantity_label" class="market_sell_dialog_field_label">Quantity:</span><input type="text" name="market_sell_quantity_input" class="market_dialog_input" id="market_sell_quantity_input"><span id="market_sell_quantity_available">of <span id="market_sell_quantity_available_amt"></span></span><span class="market_sell_dialog_field_label" id="market_price_label">You receive:</span><input type="text" name="market_sell_currency_input" class="market_dialog_input" id="market_sell_currency_input"><span class="market_sell_dialog_field_label" id="market_buyerprice_label">Buyer pays:</span><input type="text" name="market_sell_currency_input" class="market_dialog_input" id="market_sell_buyercurrency_input"><span id="market_price_includesfees">(includes fees)</span></div><br/><a id="market_sell_dialog_accept" href="#" class="btn_green_white_innerfade btn_small_wide"><span>OK, put it up for sale</span></a><div id="marker_sell_dialog_terms"><div id="market_sell_dialog_accept_ssa_container"><input id="market_sell_dialog_accept_ssa" type="checkbox" checked="1" value="0" name="accept_ssa"><label id="market_sell_dialog_accept_ssa_label" for="market_sell_dialog_accept_ssa">I agree to the terms of the Steam Subscriber Agreement</label></div></div><div style="clear:both"></div></div><div id="market_sell_dialog_error"></div></div></div><div id="market_sell_dialog_confirm_buttons" style="display: none"><div class="market_dialog_bottom_buttons"><a id="market_sell_dialog_ok" href="#" class="btn_green_white_innerfade btn_medium_wide"><span>OK</span></a><a id="market_sell_dialog_back" href="#" class="btn_grey_white_innerfade btn_medium_wide"><span>Back</span></a><div style="clear: both"></div></div></div></div><div id="market_sell_dialog_background"></div></div>'
-	);
-	//// Remove listing before set new price
-	//	new function for [OK] button
-	window.SellItemDialog.OnConfirmationAccept_new = function(){
-		window.$('market_sell_dialog_ok').stopObserving();
-		if(listingid)
-			new window.Ajax.Request('http://steamcommunity.com/market/removelisting/', {
-				method: 'post',
-				parameters: {
-					sessionid: window.g_sessionID,
-					listingid: listingid
-				},
-				onComplete: function() {
-					listingid = false;
-					return window.SellItemDialog.OnConfirmationAccept.apply(window.SellItemDialog, arguments);
-				}
-			});
-	}
-	window.$J('#market_sell_dialog_ok').click(window.SellItemDialog.OnConfirmationAccept_new);
-	*/
 	
 	
 	//// Remove button
 	// add
-	var el = document.querySelector('#tabContentsMyListings .market_listing_edit_buttons').innerHTML='<a href="#removeListings" id="btnRemoveListings" class="item_market_action_button item_market_action_button_green"><span class="item_market_action_button_edge item_market_action_button_left"></span><span class="item_market_action_button_contents">Удалить выбранные</span><span class="item_market_action_button_edge item_market_action_button_right"></span><span class="item_market_action_button_preload"></span></a>';
+	var el = document.querySelector('#tabContentsMyListings .market_listing_edit_buttons').innerHTML='<a href="#checkAllListings" id="btnCheckAllListings" class="item_market_action_button item_market_action_button_blue"><span class="item_market_action_button_edge item_market_action_button_left"></span><span class="item_market_action_button_contents">Выбрать все</span><span class="item_market_action_button_edge item_market_action_button_right"></span></a> <a href="#removeListings" id="btnRemoveListings" class="item_market_action_button item_market_action_button_green"><span class="item_market_action_button_edge item_market_action_button_left"></span><span class="item_market_action_button_contents">Удалить выбранные</span><span class="item_market_action_button_edge item_market_action_button_right"></span></a>';
 	
 	// set function
+	window.$J('#btnCheckAllListings').click(function(){
+		window.$J('.lfremove').attr('checked',!window.$J('.lfremove:first')[0].checked)
+		return false;
+	});
 	window.$J('#btnRemoveListings').click(function(){
 		var data = [];
 		
@@ -95,18 +76,12 @@ function mainPage(tabContentsMyListings){
 			market_name = window.g_rgAssets[res[2]][res[3]][res[4]].market_name;
 			
 			window.$J(el).before('<span class="item_market_action_button_contents"><input type="checkbox" class="lfremove" data-listingid="'+res[1]+'"/></span>');
-			//window.$J(el).before('<a class="editprice" href="#editprice" data-listingid="'+res[1]+'" data-app="'+res[2]+'" data-context="'+res[3]+'" data-id="'+res[4]+'">Выставить с новой ценой</a>');
+
 			window.$J(el).remove();
 		}
 	});
 
-	/*
-	window.$J('a.editprice').click(function(){
-		$t = window.$J(this);
-		listingid = $t.data('listingid');
-		window.SellItemDialog.Show( window.g_rgAssets[$t.data('app')][$t.data('context')][$t.data('id')] );
-	});
-	*/
+
 
 }
 
