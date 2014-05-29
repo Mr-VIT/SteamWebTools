b7fad021ddffe39c2454286ed98881df5163ded4
Vitaliy <vit@v1t.su>
2014-05-30 01:11:06 +0400

    0.4.6
    все скрипты подгружаются из CDN GitHub для всех браузеров

--- ext_src_chrome/includes/steam-store-launcher.js
+++
@@ -1,9 +0,0 @@
-(function(){var url = 'steam-store';
-url = chrome.extension.getURL("includes/"+url+".js");
-var script = document.createElement('script');
-script.setAttribute('type', 'text/javascript')
-script.setAttribute('src', url);
-window.addEventListener("DOMContentLoaded", function(){
-	document.getElementsByTagName('head')[0].appendChild(script);
-}, false);
-})()
