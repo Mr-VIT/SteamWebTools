19ff061a40203983a4745562f5710cb663dfdd33
Vitaliy <vit@v1t.su>
2015-10-12 16:13:27 +0300

    0.5.2
    + Нумерация предметов на Торг.Площадке
    * Быстрое скрытие/показ дубликатов. Работает как обычный фильтр
    + Возможность зафиксировать Меню навигации

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.5.1
-// @date		2015-10-08
+// @version		0.5.2
+// @date		2015-10-12
 // @downloadURL	https://mr-vit.github.io/SteamWebTools/steamwebtools.user.js
 // ==/UserScript==
