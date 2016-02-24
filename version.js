db6956bb9aecdaca1187252338de1216ab741043
Vitaliy <vit@v1t.su>
2016-02-24 22:49:46 +0300

    0.5.6 update for old version

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.5.5
-// @date		2015-12-23
+// @version		0.5.6
+// @date		2016-02-24
 // @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.user.js
 // ==/UserScript==
