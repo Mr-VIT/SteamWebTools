9b9ec288d288ca52fdb25b6fd95eef3f134c3077
Vitaliy <vit@v1t.su>
2016-03-27 22:22:29 +0300

    0.5.8

--- release/version.js
+++ release/version.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.5.7
-// @date		2016-03-11
+// @version		0.5.8
+// @date		2016-03-27
 // @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.user.js
 // ==/UserScript==
