4bd4d855ba0f99083720936bcaebe19dd67e6f44
Vitaliy <vit@v1t.su>
2016-04-20 17:18:45 +0300

    delete old version.js

--- version.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name 	Steam Web Tools
-// @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.5.6
-// @date		2016-02-24
-// @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.user.js
-// ==/UserScript==
