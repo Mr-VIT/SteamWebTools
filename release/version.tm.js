b3c1a2e1ec63843a407c05bbe12e4e148d307b28
Vitaliy <vit@v1t.su>
2016-10-04 23:42:15 +0300

    release 0.8

--- release/version.tm.js
+++ release/version.tm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.7
-// @date		2016-06-21
+// @version		0.8
+// @date		2016-10-04
 // @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.tm.user.js
 // ==/UserScript==
