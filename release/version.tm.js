cb57851420ef60bf40c23bb9ba4e4a5ec7f4c42e
root <vit@v1t.su>
2017-01-01 19:29:18 +0300

    0.8.6
    +"set lowest price" button in "Sell Item Dialog"
    + store app button: Search cards on Market
    * disable script with old economy.js
    * fix item counter for multi contexts inv-s

--- release/version.tm.js
+++ release/version.tm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.8.5
-// @date		2016-12-10
+// @version		0.8.6
+// @date		2017-01-01
 // @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.tm.user.js
 // ==/UserScript==
