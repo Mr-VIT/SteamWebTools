9d135379e36d7c3a3dc35e742f7062016bdb2f38
Mr-VIT <vit@v1t.su>
2017-05-04 16:51:48 +0300

    * fix check friend for gift

--- release/version.tm.js
+++ release/version.tm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.8.8
-// @date		2017-03-14
+// @version		0.8.9
+// @date		2017-05-04
 // @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.tm.user.js
 // ==/UserScript==
--- src/purchase.js
+++ src/purchase.js
@@ -6,8 +6,15 @@ if(W.location.search.indexOf('purchasetype=gift')>-1){
 		var curSteamId64;
 		if(curSteamId64=document.cookie.match(/steamRememberLogin=(\d{17})/)){
 			curSteamId64 = curSteamId64[1];
-			var acid=parseInt(curSteamId64.substr(-10),10)-7960265728;
-			W.$J('#current_friend_choice').after('<div class="cart_send_choice checkout_content"><input class="send_choice_radio" type="radio" id="send_self" name="send_method" onchange="if(this.checked)SelectGiftRecipient('+acid+',\'\');CheckFriendDisplay();"><label for="send_self" class="send_choice_txt"> '+t('toInventory')+'</label></div>');
+			var curacid=parseInt(curSteamId64.substr(-10),10)-7960265728;
+			var SelectGiftRecipient_old = W.SelectGiftRecipient;
+			W.SelectGiftRecipient = function(accid, name){
+				if(accid!=curacid){
+					W.$('send_self').checked=false;
+				}
+				return SelectGiftRecipient_old.apply(this, arguments);
+			}
+			W.$J('#current_friend_choice').after('<div class="cart_send_choice checkout_content"><input class="send_choice_radio" type="radio" id="send_self" name="send_method" onchange="if(this.checked)SelectGiftRecipient('+curacid+',\'\');CheckFriendDisplay();"><label for="send_self" class="send_choice_txt"> '+t('toInventory')+'</label></div>');
 		}
 	}
 	W.$('send_self').checked=true;
