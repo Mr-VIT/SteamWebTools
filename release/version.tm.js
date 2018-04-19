7c5482e0b5469f408fdc75136af7b023ff84557f
Mr-VIT <vit@v1t.su>
2018-04-19 18:33:22 +0300

    0.8.11 (TM only)
    fix broken funcs

--- release/version.tm.js
+++ release/version.tm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.8.10
-// @date		2018-03-08
+// @version		0.8.11
+// @date		2018-04-19
 // @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.tm.user.js
 // ==/UserScript==
