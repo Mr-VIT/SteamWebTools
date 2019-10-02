c92c681f349bc57daec9522fef65c09d80592e2a
Mr-VIT <vit@v1t.su>
2019-10-02 21:06:11 +0300

    0.8.14
    fixed loading settings page

--- release/version.tm.js
+++ release/version.tm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.8.13
-// @date		2019-02-25
+// @version		0.8.14
+// @date		2019-10-02
 // @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.tm.user.js
 // ==/UserScript==
