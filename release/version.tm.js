463ea77a8b4003f15fcb9569fab020a0c201e4cd
Mr-VIT <vit@v1t.su>
2020-04-29 03:46:51 +0300

    0.8.16
    + search in item tags
    + Сonfirmation for craft all available badges
    + new logos for Store items
    
    - feature "Hide acc name" (now steam shows nick)

--- release/version.tm.js
+++ release/version.tm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 	Steam Web Tools
 // @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.8.15
-// @date		2020-02-08
+// @version		0.8.16
+// @date		2020-04-29
 // @downloadURL	https://raw.githubusercontent.com/Mr-VIT/SteamWebTools/master/release/steamwebtools.tm.user.js
 // ==/UserScript==
