1017ec17fc288e03628d1dd82e15d09d3efff3bb
Mr-VIT <vit@v1t.su>
2020-12-20 23:59:02 +0300

    + add bundle to cart by id

--- src/cart-page.js
+++ src/cart-page.js
@@ -11,7 +11,7 @@ function createBlock(title, links){
 		out+='<a class="btn_small btnv6_blue_hoverfade" href="'+link.href+'"'+(link.blank ? ' target="_blank"':'')+'><span>'+link.text+'</span></a><br/>'
 	}
 
-	out+='<br/><h2>'+t('addSubidsToCart')+'</h2> <form id="addtocartsubids" method="post"><input type="hidden" name="sessionid" value="'+W.g_sessionID+'"><input type="hidden" name="action" value="add_to_cart"><input type="text" name="subids" placeholder="1, 2, 3"/><input type="submit" value="'+t('add')+'" class="btn_small btnv6_blue_hoverfade"></form><br><form id="formcarthist" method="post"><input type="submit" value="'+t('addChecked')+'" style="float:right" class="btn_small btnv6_blue_hoverfade"><h2>'+t('cartHist')+'</h2><input type="hidden" name="sessionid" value="'+W.g_sessionID+'"><input type="hidden" name="action" value="add_to_cart">';
+	out+='<br/><h2>'+t('addSubidsToCart')+'</h2> <form id="addtocartsubids" method="post"><input type="hidden" name="sessionid" value="'+W.g_sessionID+'"><input type="hidden" name="action" value="add_to_cart"><input type="text" name="subids" placeholder="1, 2, 3"/><input type="submit" value="'+t('add')+'" class="btn_small btnv6_blue_hoverfade"></form><label><input type="checkbox" id="swt_cb_asbundleid">Bundle Id</label><br><form id="formcarthist" method="post"><input type="submit" value="'+t('addChecked')+'" style="float:right" class="btn_small btnv6_blue_hoverfade"><h2>'+t('cartHist')+'</h2><input type="hidden" name="sessionid" value="'+W.g_sessionID+'"><input type="hidden" name="action" value="add_to_cart">';
 
 	var cartHistory = W.localStorage['swtcarthistory'] && JSON.parse(W.localStorage['swtcarthistory']) || [];
 	var chStr = '';
@@ -35,7 +35,14 @@ links = [
 el.insertAdjacentHTML('afterBegin', createBlock('Steam Web Tools', links));
 
 $('#addtocartsubids').bind('submit',function(){
-	var t = $(this),
+	var t = $(this);
+
+	if(t.next().find('#swt_cb_asbundleid').prop('checked')){
+		t.find('input[name="subids"]').prop('name', 'bundleid');
+		return;
+	}
+
+	var
 		subids = t.find('input[name="subids"]').val(),
 		s,
 		cartHistory = W.localStorage['swtcarthistory'] && JSON.parse(W.localStorage['swtcarthistory']) || [];
