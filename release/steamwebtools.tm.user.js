c09b684db61a6c968c41f665992a49b51ddd948b
Mr-VIT <vit@v1t.su>
2020-12-21 06:09:32 +0300

    fix updating for Tampermonkey

---
+++ release/steamwebtools.tm.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name		Steam Web Tools
+// @namespace	http://v1t.su/projects/steam/webtools/
+// @description	Useful tools in Steam web sites
+// @author		Mr-VIT
+// @homepageURL	http://v1t.su/projects/steam/webtools/
+// @icon		http://mr-vit.github.io/SteamWebTools/icon-64.png
+// @run-at		document-end
+// @include		http://store.steampowered.com/*
+// @include		https://store.steampowered.com/*
+// @include		http://steamcommunity.com/*
+// @include		https://steamcommunity.com/*
+// @exclude		https://steamcommunity.com/dev*
+// @version		0.9.1
+// @date		2020-12-20
+// @connect		checkrep.ru
+// @domain		checkrep.ru
+// @connect		store.steampowered.com
+// @domain		store.steampowered.com
+// @updateURL	https://github.com/Mr-VIT/SteamWebTools/releases/latest/download/version.tm.js
+// @downloadURL https://github.com/Mr-VIT/SteamWebTools/releases/latest/download/steamwebtools.tm.user.js
+// @grant		GM_xmlhttpRequest
+// @grant		GM_xhr
+// @grant		GM_getValue
+// @grant		GM_setValue
+// @grant		GM_deleteValue
+// ==/UserScript==
--- src/meta.tm.js
+++ src/meta.tm.js
@@ -3,6 +3,7 @@
 // @connect		store.steampowered.com
 // @domain		store.steampowered.com
 // @updateURL	https://github.com/Mr-VIT/SteamWebTools/releases/latest/download/version.tm.js
+// @downloadURL https://github.com/Mr-VIT/SteamWebTools/releases/latest/download/steamwebtools.tm.user.js
 // @grant		GM_xmlhttpRequest
 // @grant		GM_xhr
 // @grant		GM_getValue
