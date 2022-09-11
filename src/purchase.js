f3873bd875146db0baa480202a4952e7f6ea6e85
Mr-VIT <vit@v1t.su>
2022-09-11 23:44:52 +0300

    *option purchaset to inv

--- src/purchase.js
+++ src/purchase.js
@@ -3,23 +3,21 @@ W.$('accept_ssa').checked=true;
 //W.$('verify_country_only').checked=true;
 
 // == Feature == add option "To inventory" and select it
-if(W.location.search.indexOf('purchasetype=gift')>-1){
+if(W.location.search.includes('purchasetype=gift')){
 	if(!W.$('send_self')){
-		var curSteamId64;
-		if(curSteamId64=document.cookie.match(/steamRememberLogin=(\d{17})/)){
-			curSteamId64 = curSteamId64[1];
-			var curacid=parseInt(curSteamId64.substr(-10),10)-7960265728;
-			var SelectGiftRecipient_old = W.SelectGiftRecipient;
-			W.SelectGiftRecipient = function(accid, name){
-				if(accid!=curacid){
-					W.$('send_self').checked=false;
+		W.$J.ajax('/account/')
+		.done(function(responseText) {
+			var curacid = responseText.match(/g_AccountID = (\d+);/)?.[1];
+			if(curacid){
+				var SelectGiftRecipient_old = W.SelectGiftRecipient;
+				W.SelectGiftRecipient = function(accid, name){
+					if(accid!=curacid){
+						W.$('send_self').checked=false;
+					}
+					return SelectGiftRecipient_old.apply(this, arguments);
 				}
-				return SelectGiftRecipient_old.apply(this, arguments);
+				W.$J('#current_friend_choice').after('<div class="cart_send_choice checkout_content"><input class="send_choice_radio" type="radio" id="send_self" name="send_method" onchange="if(this.checked)SelectGiftRecipient('+curacid+',\'\');CheckFriendDisplay();"><label for="send_self" class="send_choice_txt"> '+t('toInventory')+'</label></div>');
 			}
-			W.$J('#current_friend_choice').after('<div class="cart_send_choice checkout_content"><input class="send_choice_radio" type="radio" id="send_self" name="send_method" onchange="if(this.checked)SelectGiftRecipient('+curacid+',\'\');CheckFriendDisplay();"><label for="send_self" class="send_choice_txt"> '+t('toInventory')+'</label></div>');
-		}
+		});
 	}
-	var rb = W.$('send_self');
-	rb.checked=true;
-	rb.onchange();
 }
