e30104a6b22b98342067c5983fa461755400c5e8
Mr-VIT <vit@v1t.su>
2022-09-14 20:44:03 +0300

    *improved fixed menu

--- src/global.js
+++ src/global.js
@@ -3,25 +3,31 @@ W.$J('.submenu_community').append('<a class="submenuitem swt" href="https://stea
 
 // == Feature == fix top navbar & to top button
 if(settings.cur.globalFixNavbar){
-	W.$J('head').append('<style>#global_header.swtfixed{position:fixed;z-index:9999;width:100%}.responsive_page_template_content.swtfixed{padding-top:50px}#global_header.swtfixed>.content{height:50px !important}#global_header.swtfixed div.logo{display:none}#global_header.swtfixed .menuitem{padding-top:15px !important}#global_header.swtfixed .supernav_container{left:0 !important}#global_header.swtfixed .header_installsteam_btn{display:none}.swtup{display:none}#global_header.swtfixed .swtup{display:block;float:left}</style>');
-	
-	
+	W.$J('head').append('<style>#global_header.swtfixed{position:fixed;z-index:998;width:100%}.responsive_page_template_content.swtfixed{padding-top:var(--headerheight)}#global_header.swtfixed>.content{height:50px!important}.swtup,#global_header.swtfixed .header_installsteam_btn,#global_header.swtfixed div.logo{display:none}#global_header.swtfixed .menuitem{padding-top:15px!important}#global_header.swtfixed .supernav_container{left:0!important}#global_header.swtfixed .swtup{display:block;float:left}</style>');
+
+
 	var menu = W.$J('#global_header');
 	if(menu.length){
 		menu.prepend('<a class="swtup" href="#"><span class="btn_grey_white_innerfade btn_medium_wide"><span>'+t('totop')+'</span></span></a>');
 		var origOffsetY = menu.offset().top;
 
-		function scroll() {
-			if (W.$J(window).scrollTop() > origOffsetY) {
-				W.$J('#global_header').addClass('swtfixed');
-				W.$J('.responsive_page_template_content').addClass('swtfixed');
+		var cssClassName = 'swtfixed',
+			header = W.$J('#global_header'),
+			pageContent = W.$J('.responsive_page_template_content');
+
+		pageContent[0].style.setProperty("--headerheight", header[0].clientHeight+"px");
+		$w = W.$J(window);
+
+		W.document.onscroll = ()=>{
+			if ($w.scrollTop() > origOffsetY) {
+				header.addClass(cssClassName);
+				pageContent.addClass(cssClassName);
 			} else {
-				W.$J('#global_header').removeClass('swtfixed');
-				W.$J('.responsive_page_template_content').removeClass('swtfixed');
+				header.removeClass(cssClassName);
+				pageContent.removeClass(cssClassName);
 			}
 
-		}
-		W.document.onscroll = scroll;
+		};
 	}
 }
 
