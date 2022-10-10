2a2623d374f916ec2c842644db113a041cf7a1bf
Mr-VIT <vit@v1t.su>
2022-10-10 21:06:26 +0300

    run the script actually at the end

--- src/meta.js
+++ src/meta.js
@@ -5,9 +5,7 @@
 // @author		Mr-VIT
 // @homepageURL	http://v1t.su/projects/steam/webtools/
 // @icon		http://mr-vit.github.io/SteamWebTools/icon-64.png
-// @run-at		document-end
-// @include		http://store.steampowered.com/*
-// @include		https://store.steampowered.com/*
-// @include		http://steamcommunity.com/*
-// @include		https://steamcommunity.com/*
+// @run-at		document-idle
+// @match		*://store.steampowered.com/*
+// @match		*://steamcommunity.com/*
 // @exclude		https://steamcommunity.com/dev*
