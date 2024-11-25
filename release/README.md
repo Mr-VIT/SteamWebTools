1d42f25eda6d32bb54c3e2450dc4f0635893b5aa
Mr-VIT <vit@v1t.su>
2024-11-25 20:47:27 +0300

    obsolete

--- release/README.md
+++
@@ -1,3 +0,0 @@
-The releases are now there - https://github.com/Mr-VIT/SteamWebTools/releases
-
-`version.tm.js`, `steamwebtools.tm.user.js` files are kept for updates 0.9 and earlier releases.
--- release/version.tm.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name 	Steam Web Tools
-// @namespace	http://v1t.su/projects/steam/webtools/
-// @version		0.10
-// @date		2020-12-21
-// @downloadURL https://github.com/Mr-VIT/SteamWebTools/releases/latest/download/steamwebtools.tm.user.js
-// ==/UserScript==
